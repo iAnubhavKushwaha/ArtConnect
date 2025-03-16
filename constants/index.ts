692a89dda1638462d19932b4e20b53afd9d48f2a
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-03-16 23:44:04 +0530

    Initialised project and Completed Authentication using clerk

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const headerLinks = [
+  {
+    label: "Home",
+    route: "/",
+  },
+  {
+    label: "Create Event",
+    route: "/events/create",
+  },
+  {
+    label: "My Profile",
+    route: "/profile",
+  },
+];
+
+export const eventsDefaultValues = {
+  title: "",
+  description: "",
+  location: "",
+  imageUrl: "",
+  startDateTime: "",
+  endDateTime: "",
+  categoryId: "",
+  price: "",
+  isFree: "",
+  url: "",
+};
