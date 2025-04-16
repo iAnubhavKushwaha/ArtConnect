74bff03cf02ca86cad8f658c72ae0e1fc3c81c1c
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-04-17 00:57:55 +0530

    Little change in user schema validation

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
     const user = {
         clerkId: id,
         email: email_addresses[0].email_address,
-        username: username!,
+        username: username || email_addresses[0].email_address.split('@')[0], // ✅ fallback
         firstName: first_name!,
         lastName: last_name!,
         photo: image_url,
