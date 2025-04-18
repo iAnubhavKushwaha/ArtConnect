29a609e7fd98345e55c4c6b0d794bc745d41c45a
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-04-18 23:11:59 +0530

    Event card with update and delete actions

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 
 const EventDetails = async ({ params: { id } }: SearchParamProps) => {
   const event = await getEventById(id);
-  console.log(event);
 
   return (
     <section className="flex justify-center bg-purple-50 bg-dotted-pattern bg-contain">
