2e8692d2bf8d62f1e3fae172c96c4f426f7278c3
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-04-20 19:32:21 +0530

    Category Filter Functionality working

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -6,39 +7,39 @@ import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-
-export default async function Home({ searchParams}: SearchParamProps) {
-
+export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
   const searchText = (searchParams?.searchTerm as string) || "";
   const category = searchParams?.category as string;
-
-
 
   const events = await getAllEvents({
     query: searchText,
     category,
     page,
     limit: 6,
-  })
-
-  console.log(events)
-
+  });
   return (
     <>
       <section className="bg-purple-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Magic!</h1>
-            <p className="p-regular-20 md:p-regular-24">Unlock expert advice from 3,168+ mentors at world-class companies—your global community awaits!</p>
-            <Button size="lg" asChild className="button w-full sm:w-fit bg-purple-500">
-              <Link href="#events">
-                Explore Now
-              </Link>
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Magic!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Unlock expert advice from 3,168+ mentors at world-class
+              companies—your global community awaits!
+            </p>
+            <Button
+              size="lg"
+              asChild
+              className="button w-full sm:w-fit bg-purple-500"
+            >
+              <Link href="#events">Explore Now</Link>
             </Button>
           </div>
 
-          <Image 
+          <Image
             src="/assets/images/hero2.jpg"
             alt="hero"
             width={1000}
@@ -46,19 +47,19 @@ export default async function Home({ searchParams}: SearchParamProps) {
             className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh] rounded-2xl"
           />
         </div>
-      </section> 
+      </section>
 
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
         <h2 className="h2-bold">
-            Trusted with ❤️ <br/> Thousands of Events
+          Trusted with ❤️ <br /> Thousands of Events
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          <h3 className="font-bold">
-            <Search/>
-          </h3>
-          CategoryFilter
-
+          <Search />
+          <CategoryFilter/>
         </div>
 
         <Collection
@@ -69,8 +70,7 @@ export default async function Home({ searchParams}: SearchParamProps) {
           limit={6}
           page={1}
           totalPages={2}
-          />
-
+        />
       </section>
     </>
   );
