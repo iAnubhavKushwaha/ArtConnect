cc435c97a2cd6b11154c38821e2d979f82d29928
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-04-22 00:07:01 +0530

    more UI fixes

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -23,7 +23,7 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
           <Image
-            src="/assets/images/logo2.svg"
+            src="/assets/images/ArtConnect-logo.svg"
             alt="logo"
             width={124}
             height={38}
