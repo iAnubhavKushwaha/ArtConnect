2278816393b22525eb4e6187230819c99f076f84
Anubhav kushwaha <130231349+iAnubhavKushwaha@users.noreply.github.com>
2025-04-22 00:20:54 +0530

    minor code fixes

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -70,7 +70,7 @@ const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
           ))}
 
         <AlertDialog>
-          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Open</AlertDialogTrigger>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Add new category</AlertDialogTrigger>
           <AlertDialogContent className="bg-white">
             <AlertDialogHeader>
               <AlertDialogTitle>New Category</AlertDialogTitle>
